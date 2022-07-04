3a3c7247a752b5576bb55d767f2f52af25a3f673
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2022-07-04 11:39:23 +0200

    Added function to create query to create a NFT

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -12,6 +12,7 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
 
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
+    mapping(uint256 => uint256) public tokenDNA;
 
     constructor(uint256 _maxSupply) ERC721("PlatziPunks", "PLPKS") {
         maxSupply = _maxSupply;
@@ -21,10 +22,55 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
         uint256 current = _idCounter.current();
         
         require(current < maxSupply, "No more PlatziPunks left.");
+        
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
         _safeMint(msg.sender, current);
         
         _idCounter.increment();
     }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataaars.io/";
+    }
+
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+    string memory params;
+
+    {
+        params = string(
+            abi.encodePacked(
+                "accessoriesType=",
+                getAccessoriesType(_dna),
+                "&clotheColor=",
+                getClotheColor(_dna),
+                "&clotheType=",
+                getClotheType(_dna),
+                "&eyeType=",
+                getEyeType(_dna),
+                "&eyebrowType=",
+                getEyeBrowType(_dna),
+                "&facialHairColor=",
+                getFacialHairColor(_dna),
+                "&facialHairType=",
+                getFacialHairType(_dna),
+                "&hairColor=",
+                getHairColor(_dna),
+                "&hatColor=",
+                getHatColor(_dna),
+                "&graphicType=",
+                getGraphicType(_dna),
+                "&mouthType=",
+                getMouthType(_dna),
+                "&skinColor=",
+                getSkinColor(_dna),
+                "&topType=",
+                getTopType(_dna)
+            )
+        );
+    }
+
+    return params;
+}
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         require(_exists(tokenId), "ERC721 Metadata: URI query for nonexisting token.");
